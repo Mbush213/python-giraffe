144a7bab37cf2bae8fb27d1b8c60fefeb017a730
Mark H. B. <mmbush2010@gmail.com>
2025-10-07 16:39:34 -0600

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 character_name = "John"
 charatcer_age = "35"
-print("There once was a man named John, ")
+print("There once was a man named " + character_name + ", ")
 print("he was 35 years old. ")
-print("He really liked the name John, ")
+print("He really liked the name " + character_name + ", ")
 print("but didn't like being 35.")
