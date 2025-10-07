36467231b31f79477c81ac2ea3e9135ea4f396cc
Mark H. B. <mmbush2010@gmail.com>
2025-10-07 16:34:24 -0600

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+print("There once was a man named John, ")
+print("he was 35 years old. ")
+print("He really liked the name John, ")
+print("but didn't like being 35.")
