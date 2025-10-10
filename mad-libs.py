2ef230d13161445aab77c08c4b9b5749915b62eb
Mark H. B. <mmbush2010@gmail.com>
2025-10-10 13:25:31 -0600

    mad-libs.py

--- mad-libs.py
+++ mad-libs.py
@@ -1,4 +1,8 @@
 
-print("Roses are {color}")
-print("{plural noun} are blue")
-print("I love {celebrity}")
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
