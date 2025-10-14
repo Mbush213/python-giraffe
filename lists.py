afba97ef0285675ccdcc18ca6210af106cae9212
Mark H. B. <mmbush2010@gmail.com>
2025-10-14 12:52:25 -0600

    lists.py

--- lists.py
+++ lists.py
@@ -1,4 +1,5 @@
 
+lucky_numbers = [4, 8, 15, 16, 23, 42]
 friends = ["Kevin", "Karen", "Jim", "Oscar", "Tony"]
-friends[1] = "Mike"
-print (friends[1:3])
+friends.extend(lucky_numbers)
+print (friends)
