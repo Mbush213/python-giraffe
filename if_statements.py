39f59924ec01e19c653cb7c75b4382acb8b48710
Mark H. B. <mmbush2010@gmail.com>
2025-10-16 14:23:59 -0600

    if_statements.py

--- if_statements.py
+++ if_statements.py
@@ -4,6 +4,10 @@ is_tall = True
 
 if is_male and is_tall:
     print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a short male")
+elif not(is_male) and (is_tall):
+    print("you are not a male but are tall")
 else:
-    print("You are either not male or not tall or both")
+    print("You are not a male and not tall")
 
