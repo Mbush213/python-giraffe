602292f161b11e63e37b871a47cd592c8c8ee96e
Mark H. B. <mmbush2010@gmail.com>
2025-10-16 15:21:08 -0600

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "April": "April",
+    "May": "May",
+    "Jun": "June",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions["Nov"])
